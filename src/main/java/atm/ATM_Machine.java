1c9af1be06fdd3072ccb5334d07e13ba771c7edc
Katleho Lephallo <katleholephallo1@gmail.com>
2024-03-28 22:21:38 +0200

    initial commit

---
+++ src/main/java/atm/ATM_Machine.java
@@ -0,0 +1,18 @@
+package atm;
+
+public class ATM_Machine implements Bank{
+    @Override
+    public void checkBalance() {
+
+    }
+
+    @Override
+    public void deposit(int amount) {
+
+    }
+
+    @Override
+    public void withdraw(int amount) {
+
+    }
+}
---
+++ src/main/java/atm/Bank.java
@@ -0,0 +1,8 @@
+package atm;
+
+public interface Bank {
+
+    void checkBalance();
+    void deposit(int amount);
+    void withdraw(int amount);
+}
